8bf8ddfc24c12a32825ecbf4abb4737fd05f9d19
kato-ops <deepsea_turtle@icloud.com>
2024-12-11 16:58:17 +0900

    spreadsheet end mockup

--- next-spreadsheet/src/pages/spreadsheet.tsx
+++ next-spreadsheet/src/pages/spreadsheet.tsx
@@ -1,36 +1,7 @@
-import styles from "./Spreadsheet.module.css"
+import Spreadsheet from "@/components/Spreadsheet";
 
-export default function Spreadsheet() {
+export default function SpreadsheetPage() {
   return (
-    <div className={styles.container}>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>A</th>
-            <th>B</th>
-            <th>C</th>
-          </tr>
-          <tr>
-            <th>1</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <th>2</th>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-          </tr>
-          <tr>
-            <th>3</th>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  )
-}
+    <Spreadsheet />
+  );
+};
