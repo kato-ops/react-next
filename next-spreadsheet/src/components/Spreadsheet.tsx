2fc659e2761cc0c182568a31dbf8c4ccac4e291c
kato-ops <deepsea_turtle@icloud.com>
2024-12-12 17:49:49 +0900

    spreadSeet raw add func ...

--- next-spreadsheet/src/components/Spreadsheet.tsx
+++ next-spreadsheet/src/components/Spreadsheet.tsx
@@ -1,37 +1,68 @@
+import React, { useState } from "react";
+
+import { CellContent } from "@/types/spreadsheet";
+
 import styles from "@/styles/Spreadsheet.module.css";
 import Cell from "./cell";
 
 export default function Spreadsheet() {
+    const [cellContents, setCellContents] = useState<Array<Array<CellContent>>>([
+        [1, 2, 3,],
+        [4, 5, 6,],
+        [7, 8, 9,],
+    ]);
+
+    const updateCell = (updated: CellContent, rowIndex: number, cellIndex: number) => {
+        const updatedCellContents = [...cellContents];
+        updatedCellContents[rowIndex][cellIndex] = updated;
+        setCellContents(updatedCellContents);
+    };
+
+    const addRow = () => {
+        setCellContents([
+            ...cellContents,
+            Array(cellContents[0].length).fill(0)
+        ]);
+    };
+    const removeRow = () => {
+        setCellContents(cellContents.slice(0, -1));
+    };
+
     return (
         <div className={styles.container}>
             <table>
                 <tbody>
                     <tr>
                         <th></th>
-                        <th>A</th>
-                        <th>B</th>
-                        <th>C</th>
+                        {//各列は前提として同じ長さなので、一列目の長さ分だけ見出しを生成すれば問題ない
+                        cellContents[0].map((_, i) => (
+                            <th>{String.fromCharCode(65 + i)}</th>
+                        ))}
                     </tr>
-                    <tr>
-                        <th>1</th>
-                        <Cell content={1} />
-                        <Cell content={2} />
-                        <Cell content={3} />
-                    </tr>
-                    <tr>
-                        <th>2</th>
-                        <Cell content={4} />
-                        <Cell content={5} />
-                        <Cell content={6} />
-                    </tr>
-                    <tr>
-                        <th>3</th>
-                        <Cell content={7} />
-                        <Cell content={8} />
-                        <Cell content={9} />
-                    </tr>
+                    {
+                        cellContents.map((row, rowIndex) => {
+                            return (
+                                <tr>
+                                    <th>{rowIndex + 1}</th>
+                                    {
+                                        row.map((cell, cellIndex) => (
+                                            <Cell
+                                                content={cell}
+                                                onChange={(updated: CellContent) => updateCell(updated, rowIndex, cellIndex)}
+                                            />
+                                        ))
+                                    }
+                                </tr>
+                            );
+                        })
+                    }
                 </tbody>
             </table>
+            <button onClick={addRow}>+ row</button>
+            <button onClick={removeRow}>- row</button>
+            <br />
+            <button>+ column</button>
+            <button>- column</button>
         </div>
     );
 };
--- next-spreadsheet/src/components/cell.tsx
+++ next-spreadsheet/src/components/cell.tsx
@@ -1,39 +1,48 @@
-import { useEffect, useState, KeyboardEvent } from "react";
+import { useState, KeyboardEvent } from "react";
 
 import { CellContent } from "@/types/spreadsheet";
 
 interface Props {
     content: CellContent;
+    onChange: (updated: CellContent) => void;
 };
 
-export default function Cell({ content: initialContent }: Props) {
+export default function Cell({ content: initialContent, onChange }: Props) {
     //<>はジェネリクス。定義時に枠を作ってると使用時に型指定できる。
     const [editing, setEditing] = useState<boolean>(false);
     const [content, setContent] = useState<CellContent>(initialContent);
 
-    const handleInput = () => {
+    const handleClickCell = () => {
+        if (editing === true) {
+            onChange(content);
+        }
         setEditing(!editing);
     };
-    const handleConfirme = (event: KeyboardEvent) => {
-        if (["Enter", "Escape"].includes(event.key)) {
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === "Enter") {
             setEditing(false);
+            onChange(content);
+        }
+        if (event.key === "Escape") {
+            setEditing(false);
+            setContent(initialContent);
         }
     };
 
     return (
-        <td onClick={handleInput}>
+        <td onClick={handleClickCell}>
             {
-                editing ?
+                editing ? (
                     <input
                         value={content}
                         //これでオブジェクトが出来た段階で自動的にフォーカスが当たる
                         ref={input => input ? input.focus() : undefined}
                         onClick={(e) => { e.stopPropagation() }}
-                        onKeyDown={handleConfirme}
+                        onKeyDown={handleKeyDown}
                         onChange={(e) => setContent(e.target.value)}
-                    />
-                    : content
+                    />)
+                    : initialContent
             }
-        </td>
+        </td >
     );
 };
