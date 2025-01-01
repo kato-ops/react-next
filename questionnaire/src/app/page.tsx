cb31204f02240d2349be29421c062498d4810a4d
kato-ops <deepsea_turtle@icloud.com>
2025-01-01 21:46:03 +0900

    start practice site (make submit)

--- questionnaire/src/app/page.tsx
+++ questionnaire/src/app/page.tsx
@@ -1,10 +1,26 @@
-import Link from "next/link";
+//react13以降はサーバーサイドレンダリングがデフォルトでJSを送信しないらしい
+//なので、JS(イベント関数とか)を使いたい場合は、'use client';を付ける必要がある
+'use client';
+
 
 export default function Home() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
-    <div>
-      <h1>It's Home !!!</h1>
-      <Link href="/test">[go test page]</Link>
-    </div>
+    <>
+      <h1>プログラミング学習に関するアンケート</h1>
+
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="submit"
+          value="アンケートを提出する"
+        ></input>
+      </form>
+    </>
   );
 }
